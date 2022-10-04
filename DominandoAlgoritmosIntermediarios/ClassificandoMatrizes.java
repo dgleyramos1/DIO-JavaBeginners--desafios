7c282feb68a27a4e7c56caaf8c0e8ab694a829c5
Dgley Ramos <dgleysilva@gmail.com>
2022-10-04 11:55:25 -0300

    Finish

--- DominandoAlgoritmosIntermediarios/ClassificandoMatrizes.java
+++ DominandoAlgoritmosIntermediarios/ClassificandoMatrizes.java
@@ -1,4 +1,5 @@
-import java.util.ArrayList;
+package DominandoAlgoritmosIntermediarios;
+
 import java.util.Scanner;
 
 /*
--- DominandoAlgoritmosIntermediarios/ValidacaoParenteses.java
+++ DominandoAlgoritmosIntermediarios/ValidacaoParenteses.java
@@ -1,3 +1,5 @@
+package DominandoAlgoritmosIntermediarios;
+
 import java.util.Scanner;
 
 /*
